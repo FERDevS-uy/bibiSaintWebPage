0d4e5d4907e0158de0e143dd1cd5d91cf5d98b49
Elias288 <bianchi.elias@gmail.com>
2025-05-30 16:19:16 -0300

    fix: build fsevents bug

--- code/src/config.ts
+++ code/src/config.ts
@@ -1,11 +1,10 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
 const repositoryName = "/template-static-ecommerce";
 const url = `https://ferdevs-uy.github.io${repositoryName}/`;
 
-// https://astro.build/config
-export default defineConfig({
+const pageData = {
   site: url,
   base: repositoryName,
-});
+  pageTitle: "Static Ecommerce title",
+};
+
+export { pageData as config };
